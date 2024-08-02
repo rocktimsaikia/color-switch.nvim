7de18674370b856b96db4e2566514542126e7084
Rocktim Saikia <rocktimthedev@gmail.com>
2024-08-03 04:30:52 +0530

    init

---
+++ README.md
@@ -0,0 +1,31 @@
+# color-switch.nvim
+
+Easily convert and switch between different color formats.
+
+> [!NOTE]
+> Currently, it supports switching between HEX and RGB color formats only. \
+> More formats will be supported soon. As most engineers primarily deal \
+> with these two formats, they are enabled by default.
+
+# Installation
+
+Using [packger](https://github.com/wbthomason/packer.nvim)
+
+```lua
+use {
+    "rocktimsaikia/color-switch.nvim"
+    config = function()
+        require("color-switch")
+    end
+}
+```
+
+## Usage:
+
+The plugin only exposes a single command `:ColorSwitch` to keep the API simple and easy to use.
+
+Combine this command with your preferred key mapping in normal mode:
+
+```lua
+vim.keymap.set("n", "<Leader>cs", "<Plug>ColorConvertCycle")
+```
---
+++ lua/color-switch/init.lua
@@ -0,0 +1,51 @@
+-- Define the module
+local M = {}
+
+--  #FF5733
+
+function escape_pattern(text)
+	return text:gsub("([^%w])", "%%%1")
+end
+
+function convert_hex_to_rgb(hex)
+	hex = hex:gsub("#", "")
+	local r = tonumber(hex:sub(1, 2), 16)
+	local g = tonumber(hex:sub(3, 4), 16)
+	local b = tonumber(hex:sub(5, 6), 16)
+	return string.format("rgb(%d, %d, %d)", r, g, b)
+end
+
+function convert_rgb_to_hex(rgb_color_str)
+	local r, g, b = rgb_color_str:match("rgb%((%d+),%s*(%d+),%s*(%d+)%)")
+	r = tonumber(r)
+	g = tonumber(g)
+	b = tonumber(b)
+	return string.format("#%02X%02X%02X", r, g, b)
+end
+
+function M.get_line_colors()
+	local current_line = vim.api.nvim_get_current_line()
+	local hex_color = current_line:match("#%x%x%x%x%x%x")
+	local rgb_color = current_line:match("rgb%(%s*%d+%s*,%s*%d+%s*,%s*%d+%s*%)")
+
+	if hex_color then
+		local rgb_color_code = convert_hex_to_rgb(hex_color)
+		local updated_line = current_line:gsub(hex_color, rgb_color_code)
+		vim.api.nvim_set_current_line(updated_line)
+	elseif rgb_color then
+		print("rgb found")
+		local hex_color_code = convert_rgb_to_hex(rgb_color)
+		local escaped_rgb_color = escape_pattern(rgb_color)
+		local updated_line = current_line:gsub(escaped_rgb_color, hex_color_code)
+		vim.api.nvim_set_current_line(updated_line)
+	else
+	end
+end
+
+-- Create a command that calls the HelloWorld function
+vim.api.nvim_create_user_command("HelloWorld", function()
+	M.get_line_colors()
+end, { nargs = 0 })
+
+-- Return the module
+return M
